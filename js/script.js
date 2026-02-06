df6bffe43842e3dc5f71a0817296019de016bfc1
sapan8867236637 <38177370+sapan8867236637@users.noreply.github.com>
2026-02-06 14:17:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,16 @@ function setLang(lang) {
   document.querySelectorAll("[data-en]").forEach(el => {
     el.innerText = el.getAttribute("data-" + lang);
   });
-}
 
+  // highlight active button
+  document.querySelectorAll(".lang-btn button").forEach(btn => {
+    btn.classList.remove("active");
+  });
+
+  document
+    .querySelector(`.lang-btn button[onclick="chooseLang('${lang}')"]`)
+    ?.classList.add("active");
+}
 function chooseLang(lang) {
   localStorage.setItem("siteLang", lang);
   setLang(lang);
